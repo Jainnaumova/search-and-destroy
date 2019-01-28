669fa15f2064bd2814a05538c1a4087f3e03d78d
Evgeniya Naumova <jainnaumova@gmail.com>
2019-01-28 12:53:49 -0500

    first

--- binary-search/binary-search.js
+++ binary-search/binary-search.js
@@ -1,10 +1,30 @@
-'use strict';
+"use strict";
 
 // Complete this algo
-const binarySearch = (array, target) => {
-	
-};
+// const binarySearch = (array, target) => {
+// 	for (let i = 0; i < array.length; i++) {
+// 		let curEl = array[i];
+// 		if (curEl === target) return true;
+//
+// 	}
+//   return false;
+// };
 
+// const binarySearch = (array, target) => {
+//   if (!array.length || (array.length === 1 && array[0] !== target))
+//     return false;
+//   if (array.length === 1 && array[0] === target) return true;
+//   else {
+//     let midpoint = array[Math.floor(array.length / 2)];
+//     if (target < midpoint) {
+//       array = array.slice(0, midpoint);
+//       return binarySearch(array, target);
+//     } else {
+//       array = array.slice(midpoint, array.length - 1);
+//       return binarySearch(array, target);
+//     }
+//   }
+// };
 /*
 	EXTRA CREDIT:
 
@@ -14,4 +34,4 @@ const binarySearch = (array, target) => {
 
 */
 
-module.exports = binarySearch
+module.exports = binarySearch;
